78cdbbd99f4a2ed7bb1e7d51855f5c1debad7c15
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2022-11-09 13:52:51 -0500

    changed class piece name

---
+++ Checkers/src/PlayerPieces.java
@@ -0,0 +1,13 @@
+/**
+ * This is the class for our checkers pieces ("Men")
+ **/
+
+public class PlayerPieces {
+    // member variables
+
+    // constructor
+    public PlayerPieces() {
+
+    }
+
+}
