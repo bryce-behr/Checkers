35f2e79645daa817088ac151ce60beee73228db0
DavidJPLeRoux <lerouxdj21@gcc.edu>
2022-11-14 12:41:45 -0500

    Man & Game
    Added Comments to Man and methods to Game

--- Checkers/src/Game.java
+++ Checkers/src/Game.java
@@ -6,6 +6,21 @@ public class Game {
 
     //Default Constructor
     public Game(){
+        boards = new Stack<>();
+    }
 
+    public Stack<Board> getBoards() {
+        return boards;
+    }
+    public void setBoards(Stack<Board> boards) {
+        this.boards = boards;
+    }
+
+    public Stack<Board> back(){
+        return null;
+    }
+
+    public Board currentBoard(){
+        return null;
     }
 }
--- Checkers/src/Man.java
+++ Checkers/src/Man.java
@@ -5,20 +5,73 @@
 public class Man {
     // member variables
     private int team;
+    private int x;
+    private int y;
 
-    // constructor
+    //Default Constructor
     public Man() {
          team = 0;
-    }
-    public Man(int team){
-        setTeam(team);
+         x = 0;
+         y = 0;
     }
 
+    /**
+     * Constructor
+     * @param team the team that is on that square (can be black[1], red[2] or "null"[0]
+     * @param x the x co-ordinate on the board
+     * @param y the y co-ordinate on the board
+     */
+    public Man(int team, int x, int y){
+        setTeam(team);
+        setX(x);
+        setY(y);
+    }
+
+    /**
+     *
+     * @return the y co-ordinate
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets the member variable y
+     * @param y the y co-ordinate
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     *
+     * @return the x co-ordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Sets the member variable x
+     * @param x the x co-ordinate
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     *
+     * @return What team is on that square
+     */
     public int getTeam(){
         return team;
     }
+
+    /**
+     * Sets the member variable team
+     * @param team What team is on that specific square
+     */
     public void setTeam(int team){
         this.team = team;
     }
-
 }
