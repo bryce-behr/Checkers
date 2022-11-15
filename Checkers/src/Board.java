a3f3a74863a128d8fc3247eddd886df6747e4b67
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2022-11-15 09:32:23 -0500

    Board
    just comment changes

--- Checkers/src/Board.java
+++ Checkers/src/Board.java
@@ -2,19 +2,19 @@ public class Board {
     private Square[][] positions = new Square[8][8];
 
     /**
-     * this is the board constructor
+     * this is the board constructor. It creates a board in the starting position
      **/
     public Board() {
 
     }
 
     /**
-     * this method moves a piece from one square to the next
+     * this method checks to make sure that a move is valid
      * @param oldSquare the square that you're moving from
      * @param newSquare the square that you're going to
      * @return boolean true if valid move, false if invalid
      **/
-    public boolean move(Square oldSquare, Square newSquare) {
+    public boolean checkValid(Square oldSquare, Square newSquare) {
         return true;
     }
 
@@ -23,7 +23,7 @@ public class Board {
      * this method returns a new Board with a moved piece
      * @param oldSquare the old square you want to move from
      * @param newSquare the new square you want to move to
-     * @return
+     * @return the new board which is the current state of the game
      */
     public Board makeMove(Square oldSquare, Square newSquare) {
         return null;
